3b6dd28b5544de593bbc305d01a7078468578774
IgorChizh <87984244+IgorChizh@users.noreply.github.com>
2021-08-09 09:16:05 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿
+namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnEql = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnDot = new System.Windows.Forms.Button();
+            this.resBox = new System.Windows.Forms.TextBox();
+            this.btnCle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn7.Font = new System.Drawing.Font("Broadway", 14F);
+            this.btn7.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btn7.Location = new System.Drawing.Point(40, 80);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(58, 30);
+            this.btn7.TabIndex = 0;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.button_click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn4.Font = new System.Drawing.Font("Broadway", 14F);
+            this.btn4.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btn4.Location = new System.Drawing.Point(40, 116);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(58, 30);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.button_click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn8.Font = new System.Drawing.Font("Broadway", 14F);
+            this.btn8.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btn8.Location = new System.Drawing.Point(104, 80);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(58, 30);
+            this.btn8.TabIndex = 1;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.button_click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn5.Font = new System.Drawing.Font("Broadway", 14F);
+            this.btn5.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btn5.Location = new System.Drawing.Point(104, 116);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(58, 30);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.button_click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn9.Font = new System.Drawing.Font("Broadway", 14F);
+            this.btn9.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btn9.Location = new System.Drawing.Point(168, 80);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(58, 30);
+            this.btn9.TabIndex = 2;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.button_click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn6.Font = new System.Drawing.Font("Broadway", 14F);
+            this.btn6.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btn6.Location = new System.Drawing.Point(168, 116);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(58, 30);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.button_click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn1.Font = new System.Drawing.Font("Broadway", 14F);
+            this.btn1.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btn1.Location = new System.Drawing.Point(40, 152);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(58, 30);
+            this.btn1.TabIndex = 6;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.button_click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn3.Font = new System.Drawing.Font("Broadway", 14F);
+            this.btn3.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btn3.Location = new System.Drawing.Point(168, 152);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(58, 30);
+            this.btn3.TabIndex = 8;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.button_click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn2.Font = new System.Drawing.Font("Broadway", 14F);
+            this.btn2.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btn2.Location = new System.Drawing.Point(104, 152);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(58, 30);
+            this.btn2.TabIndex = 7;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.button_click);
+            // 
+            // btn0
+            // 
+            this.btn0.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn0.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn0.Font = new System.Drawing.Font("Broadway", 14F);
+            this.btn0.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btn0.Location = new System.Drawing.Point(40, 188);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(58, 30);
+            this.btn0.TabIndex = 9;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.button_click);
+            // 
+            // btnEql
+            // 
+            this.btnEql.BackColor = System.Drawing.Color.Azure;
+            this.btnEql.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEql.Font = new System.Drawing.Font("Broadway", 16F);
+            this.btnEql.ForeColor = System.Drawing.Color.Black;
+            this.btnEql.Location = new System.Drawing.Point(139, 188);
+            this.btnEql.Name = "btnEql";
+            this.btnEql.Size = new System.Drawing.Size(87, 30);
+            this.btnEql.TabIndex = 11;
+            this.btnEql.Text = "=";
+            this.btnEql.UseVisualStyleBackColor = false;
+            this.btnEql.Click += new System.EventHandler(this.btnEql_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.Font = new System.Drawing.Font("Broadway", 16F);
+            this.btnAdd.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btnAdd.Location = new System.Drawing.Point(242, 80);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(33, 45);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.operator_click);
+            // 
+            // btnMin
+            // 
+            this.btnMin.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnMin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMin.Font = new System.Drawing.Font("Broadway", 16F);
+            this.btnMin.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btnMin.Location = new System.Drawing.Point(281, 80);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(33, 45);
+            this.btnMin.TabIndex = 13;
+            this.btnMin.Text = "-";
+            this.btnMin.UseVisualStyleBackColor = false;
+            this.btnMin.Click += new System.EventHandler(this.operator_click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnMul.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMul.Font = new System.Drawing.Font("Broadway", 16F);
+            this.btnMul.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btnMul.Location = new System.Drawing.Point(242, 131);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(33, 42);
+            this.btnMul.TabIndex = 14;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = false;
+            this.btnMul.Click += new System.EventHandler(this.operator_click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnDiv.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDiv.Font = new System.Drawing.Font("Broadway", 14F);
+            this.btnDiv.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btnDiv.Location = new System.Drawing.Point(281, 131);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(33, 42);
+            this.btnDiv.TabIndex = 15;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = false;
+            this.btnDiv.Click += new System.EventHandler(this.operator_click);
+            // 
+            // btnDot
+            // 
+            this.btnDot.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnDot.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDot.Font = new System.Drawing.Font("Broadway", 12F);
+            this.btnDot.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btnDot.Location = new System.Drawing.Point(104, 188);
+            this.btnDot.Name = "btnDot";
+            this.btnDot.Size = new System.Drawing.Size(29, 30);
+            this.btnDot.TabIndex = 10;
+            this.btnDot.Text = ",";
+            this.btnDot.UseVisualStyleBackColor = false;
+            this.btnDot.Click += new System.EventHandler(this.button_click);
+            // 
+            // resBox
+            // 
+            this.resBox.Font = new System.Drawing.Font("Broadway", 18F);
+            this.resBox.Location = new System.Drawing.Point(40, 41);
+            this.resBox.Multiline = true;
+            this.resBox.Name = "resBox";
+            this.resBox.Size = new System.Drawing.Size(274, 33);
+            this.resBox.TabIndex = 16;
+            this.resBox.Text = "Let\'s count";
+            this.resBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCle
+            // 
+            this.btnCle.BackColor = System.Drawing.Color.Azure;
+            this.btnCle.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCle.Font = new System.Drawing.Font("Broadway", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCle.ForeColor = System.Drawing.Color.Black;
+            this.btnCle.Location = new System.Drawing.Point(242, 179);
+            this.btnCle.Name = "btnCle";
+            this.btnCle.Size = new System.Drawing.Size(72, 39);
+            this.btnCle.TabIndex = 16;
+            this.btnCle.Text = "Clear";
+            this.btnCle.UseVisualStyleBackColor = false;
+            this.btnCle.Click += new System.EventHandler(this.btnCle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(359, 278);
+            this.Controls.Add(this.btnCle);
+            this.Controls.Add(this.resBox);
+            this.Controls.Add(this.btnDot);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnMin);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnEql);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnEql;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnDot;
+        private System.Windows.Forms.TextBox resBox;
+        private System.Windows.Forms.Button btnCle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        Double resultValue = 0;
+        string operatorClicked = "";
+        bool isOperatorClikced = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            if (resBox.Text == "Let's count" || (isOperatorClikced) || resBox.Text == "0")
+                resBox.Clear();
+
+            isOperatorClikced = false;
+            Button button = (Button)sender;
+
+            if (button.Text == ".")
+            {
+                if(!resBox.Text.Contains("."))
+                    resBox.Text = resBox.Text + button.Text;
+            }
+            else
+                resBox.Text = resBox.Text + button.Text;
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if(resultValue != 0)
+            {
+                btnEql.PerformClick();
+                operatorClicked = button.Text;
+                isOperatorClikced = true;
+            }
+            else
+            {
+                operatorClicked = button.Text;
+                resultValue = Double.Parse(resBox.Text);
+                isOperatorClikced = true;
+            }
+
+            operatorClicked = button.Text;
+            resultValue = Double.Parse(resBox.Text);
+        }
+
+        private void btnCle_Click(object sender, EventArgs e)
+        {
+            resBox.Text = "0";
+            resultValue = 0;
+        }
+
+        private void btnEql_Click(object sender, EventArgs e)
+        {
+            switch (operatorClicked)
+            {
+                case "+":
+                    resBox.Text = (resultValue + Double.Parse(resBox.Text)).ToString();
+                    break;
+                case "-":
+                    resBox.Text = (resultValue - Double.Parse(resBox.Text)).ToString();
+                    break;
+                case "*":
+                    resBox.Text = (resultValue * Double.Parse(resBox.Text)).ToString();
+                    break;
+                case "/":
+                    resBox.Text = (resultValue / Double.Parse(resBox.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
